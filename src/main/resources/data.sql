fc577a58026e3639cfc8694a320572e749c108fd
phk0606 <phk0606@gmail.com>
2018-02-05 00:33:38 +0900

    5-4. 수정/삭제 기능에 대한 보안 처리 및 LocalDateTime 설정

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,5 @@
-insert into user (user_id, password, name, email) values ('javajigi', '2222','자바지기', 'abc@abc.com')
-insert into user (user_id, password, name, email) values ('sanjigi', '2222','산지기', 'sanjigi@abc.com')
+insert into user (id, user_id, password, name, email) values (1, 'javajigi', '2222','자바지기', 'abc@abc.com');
+insert into user (id, user_id, password, name, email) values (2, 'sanjigi', '2222','산지기', 'sanjigi@abc.com');
+
+insert into question (id, writer_id, title, contents, create_date) values (1, 1, '자바지기가 등록한글', '자바지기가 등록했어요', current_timestamp());
+insert into question (id, writer_id, title, contents, create_date) values (2, 2, '산지기가 등록한글', '산지기가 등록했어요', current_timestamp());
