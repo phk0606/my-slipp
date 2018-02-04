fac7b025acb84be8f52c62e00e811f1eace60110
phk0606 <phk0606@gmail.com>
2018-02-04 19:33:31 +0900

    4-1 로그인 기능 구현완료

--- src/main/java/net/slipp/web/UserController.java
+++ src/main/java/net/slipp/web/UserController.java
@@ -1,5 +1,7 @@
 package net.slipp.web;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -19,9 +21,32 @@ public class UserController {
 	@Autowired
 	private UserRepository userRepository;
 	
+	@GetMapping("/loginForm")
+	public String loginForm() {
+		return "/user/login";
+	}
+	
 	@GetMapping("/form")
 	public String form() {
 		return "/user/form";
+	}
+	
+	@PostMapping("/login")
+	public String login(String userId, String password, HttpSession session) {
+		User user = userRepository.findByUserId(userId);
+		System.out.println("user: "+user);
+		if(user == null) {
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+		
+		if(!password.equals(user.getPassword())) {
+			System.out.println("Login Failure!");
+			return "redirect:/users/loginForm";
+		}
+		System.out.println("Login Success!");
+		session.setAttribute("user", user);
+		return "redirect:/";
 	}
 	
 	@PostMapping("")
---
+++ src/main/resources/data.sql
@@ -0,0 +1,1 @@
+insert into user (id, user_id, password, name, email) values (1, 'javajigi', '2222','자바지기', 'abc@abc.com')
